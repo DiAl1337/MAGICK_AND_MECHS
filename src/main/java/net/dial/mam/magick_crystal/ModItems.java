c6c543c49e7e0f19725050286f05a98b0cc6f8ed
DiAl1337 <grishkevich-1993@mail.ru>
2022-04-19 19:31:05 +0300

    Initial commit

---
+++ src/main/java/net/dial/mam/magick_crystal/ModItems.java
@@ -0,0 +1,28 @@
+package net.dial.mam.magick_crystal;
+
+import net.dial.mam.MaM;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MaM.MOD_ID);
+
+
+    public static final RegistryObject<Item> magick_crystal = ITEMS.register("magick_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+    public static final RegistryObject<Item> magickal_condencer = ITEMS.register("magickal_condencer",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
