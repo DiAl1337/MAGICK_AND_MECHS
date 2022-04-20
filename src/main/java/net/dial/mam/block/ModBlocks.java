e3d5b806045671bbbd87839db114ffa0b0bc7e15
DiAl1337 <grishkevich-1993@mail.ru>
2022-04-20 18:17:09 +0300

    Added a comment::

--- src/main/java/net/dial/mam/block/ModBlocks.java
+++ src/main/java/net/dial/mam/block/ModBlocks.java
@@ -25,6 +25,10 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f)
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.MECHS_AND_MAGIC_TAB);
 
+    public static final RegistryObject<Block> crystal_ore = registerBlock("crystal_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.MECHS_AND_MAGIC_TAB);
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
