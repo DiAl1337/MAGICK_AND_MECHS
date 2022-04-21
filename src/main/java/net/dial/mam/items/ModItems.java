c2aa80fbc60f17bb35e0f5e7db336b2e82a7ee97
DiAl1337 <grishkevich-1993@mail.ru>
2022-04-22 01:17:19 +0300

    Added a comment: Добавил crystal cloth interlaced crystal cloth large catalyst small catalyst raw mithril mithril ingot осталось ток руду и блок добавить

--- src/main/java/net/dial/mam/items/ModItems.java
+++ src/main/java/net/dial/mam/items/ModItems.java
@@ -20,6 +20,26 @@ public class ModItems {
     public static final RegistryObject<Item> magical_condencer = ITEMS.register("magical_condencer",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
 
+    public static final RegistryObject<Item> interlaced_crystal_cloth = ITEMS.register("interlaced_crystal_cloth",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+    public static final RegistryObject<Item> mithril_ingot = ITEMS.register("mithril_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+    public static final RegistryObject<Item> raw_mithril = ITEMS.register("raw_mithril",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+    public static final RegistryObject<Item> crystal_cloth = ITEMS.register("crystal_cloth",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+    public static final RegistryObject<Item> large_catalyst = ITEMS.register("large_catalyst",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+    public static final RegistryObject<Item> small_catalyst = ITEMS.register("small_catalyst",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MECHS_AND_MAGIC_TAB)));
+
+
+
 
 
     public static void register(IEventBus eventBus) {
